478668c3f02c71dc2c7b8025e38e4b570ea1d89a
Wasipo <Admin@wasipo.com>
2015-02-18 15:18:34 +0700

    Added some functions

--- app.js
+++ app.js
@@ -1,33 +1,64 @@
+//Angular Backend
 var app = angular.module('testApp', []);
 
-app.factory('Login', function($location) {
-    var login = {};
+//DataStore Service
+app.factory('DataStore', function($window) {
+   var ds = {};
+   ds.getData = function(key) {
+      return JSON.parse($window.localStorage.getItem(key));
+   };
+   ds.setData = function(key, value) {
+      $window.localStorage.setItem(key, JSON.stringify(value));
+   };
+   ds.removeKey = function(key) {
+      $window.localStorage.removeItem(key);
+   };
+   return ds;
+});
+
+//Login Controller
+app.controller('loginController', function(DataStore){
+   var vm = this;
+   var constructor = function() {
+      //Generate dummy users
+      var data = DataStore.getData('users');
+      var users = [{id:1, user:"foo", pass:"bar", name:"Foobar"},
+                   {id:2, user:"test", pass:"test", name:"Admin"}];
+      if (!data) 
+      DataStore.setData('users', users);
+   };
+   constructor(); //Call constructor
+   
+   vm.login = function(id) {
+      if (id) {
+         DataStore.setData('currentUserId', id); 
+         window.location.href = "/dash.html";
+      }     
+   };
+});
+
+app.controller('dashboardController', function(DataStore) {
+   var vm = this;
+   var constructor = function() {
+      var id = DataStore.getData('currentUserId');
+      if (!id){
+         alert('Login First');
+         window.location.href = "/index.html";
+      }
+      var query = DataStore.getData('users').filter(function(item) {
+         return (item.id == id);
+      });
+      vm.currentUser = query[0].user;
+      vm.helloMessage = 'Welcome, ' + query[0].user;
+   };
+   vm.currentUser = {};
+   vm.logout = function() {
+      DataStore.removeKey('currentUserId');
+      window.location.href = "/index.html";
+   }
+   
+   constructor(); //Call constructor
+});
     
-    login.data = "";
-    login.get = function() {
-        return this.data;
-    };
-    login.set = function(text) {
-        this.data = text;
-    };
-    return login;
-});
 
-app.controller('loginController', function(Login) {
-    var vm = this;
-    vm.text = Login.get();
-    vm.send = function() {
-        Login.set('Hello World');  
-        this.text = Login.get();
-        window.location.href = '/dash.html';
-    };
-});
 
-app.controller('dashController', function(Login) {
-    var vm = this;
-    vm.text = Login.get();
-    vm.send = function() {
-        //Login.set('Hello World');  
-        this.text = Login.get();
-    };
-});
